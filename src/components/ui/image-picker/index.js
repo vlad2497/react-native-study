3537c532ff48bee6e01c856d59154c10ab9bc456
Ideas <ideas@MacBook-Pro-Ideas.local>
2021-08-31 16:17:38 +0300

    has been added image picker

---
+++ src/components/ui/image-picker/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default ({ setSelectedImage }) => {
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      console.log("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    } else {
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+  };
+
+  return (
+    <TouchableOpacity onPress={openImagePickerAsync}>
+      <Text>Pick a photo</Text>
+    </TouchableOpacity>
+  );
+};
--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import ImagePicker from "components/ui/image-picker";
 import { styles } from "./styles";
 
 const List = () => {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Версия приложения 1.0.0</Text>
+      <ImagePicker setSelectedImage={setSelectedImage} />
+      {selectedImage ? (
+        <View>
+          <Image
+            source={{ uri: selectedImage.localUri }}
+            style={{ width: 300, height: 300, borderRadius: 150 }}
+          />
+        </View>
+      ) : null}
     </View>
   );
 };
